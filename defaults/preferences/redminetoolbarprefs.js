0b288ea4f9fdffdec9c1da882bd156b67415a2c0
fgerards <frank.gerards@gerards-gbr.de>
2013-03-06 15:38:07 +0100

    Update redminetoolbarprefs.js

--- defaults/preferences/redminetoolbarprefs.js
+++ defaults/preferences/redminetoolbarprefs.js
@@ -1,7 +1,4 @@
-pref("extensions.redminetoolbar.currentproject", "redmine");
-pref("extensions.redminetoolbar.projects.name.0", "redmine");
-pref("extensions.redminetoolbar.projects.url.0", "http://www.redmine.org");
-pref("extensions.redminetoolbar.projects.name.1", "ruby");
-pref("extensions.redminetoolbar.projects.url.1", "http://redmine.ruby-lang.org");
-pref("extensions.redminetoolbar.project.redmine.wikipage.0", "Guide");
-pref("extensions.redminetoolbar.project.redmine.wikipage.1", "HowTos");
+pref("extensions.redminetoolbar.currentproject", "Datev");
+pref("extensions.redminetoolbar.projects.name.0", "Datev");
+pref("extensions.redminetoolbar.projects.url.0", "http://ssl.former03.de/tools/redmine/kunden-datev");
+
